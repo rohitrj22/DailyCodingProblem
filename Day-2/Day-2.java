5bf138677ed4f88cfaf5921eb9d5b380c8766dcc
unknown <rohitraju360@gmail.com>
2022-02-21 01:16:59 +0530

    handle zero case

--- Day-2/Day-2.java
+++ Day-2/Day-2.java
@@ -7,16 +7,27 @@ For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120
 Follow-up: what if you can't use division? */
 
 import java.util.*;
-import java.io.*;
+
+import javax.lang.model.util.ElementScanner14;
 
 class Problem
 {
 
-    static  Integer[] solve(Integer arr[],int product)
+    static  Integer[] solve(Integer arr[],int product,int flag)
     {
         
         for(int i=0;i<arr.length;i++)
+        {
+            if(flag>1)
+                arr[i]=0;
+            else if(flag==1 && arr[i]!=0)
+                arr[i]=0;
+            else if(flag==0)
             arr[i] = product/arr[i];
+            else
+                arr[i]=product;
+        }
+            
         return arr;
     }
     
@@ -25,18 +36,21 @@ class Problem
 
         Scanner sc = new Scanner(System.in);
         List<Integer> li = new ArrayList<Integer>();
-        int product=1;
+        int product=1,flag=0;
         while(sc.hasNextInt())
         {
             int num = sc.nextInt();
-            product= product * num;
+            if(num==0)
+                flag++;
+            else
+                product= product * num;
             li.add(num);
         }
         sc.close();
         int size = li.size();
         Integer arr[] = new Integer[size];
         arr = li.toArray(new Integer[0]);
-        arr = solve(arr,product);
+        arr = solve(arr,product,flag);
         for(Integer i: arr)
             System.out.print(i + " ");
 		// your code goes here
