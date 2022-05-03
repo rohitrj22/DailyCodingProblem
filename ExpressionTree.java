782832627a03dec9a515994b0209167a2dd7f62f
rohitrj22 <rohitraju360@gmail.com>
2022-05-03 20:28:09 +0530

    add question day-50

--- ExpressionTree.java
+++ ExpressionTree.java
@@ -1,3 +1,14 @@
+// Good morning! Here's your coding interview problem for today.
+
+// This problem was asked by Microsoft.
+
+// Suppose an arithmetic expression is given as a binary tree. Each leaf is an integer and each internal node is one of '+', '−', '∗', or '/'.
+
+// Given the root to such a tree, write a function to evaluate it.
+
+// For example, given the following tree:
+
+
 class Node
 {
     String data;
